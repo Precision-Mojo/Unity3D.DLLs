c5132e179422c1fa90e89d491a94f734dbca688b
Marcus R. Brown <mrbrown@precision-mojo.com>
2013-07-01 18:24:48 -0700

    Add an installation script.

---
+++ NuGet/tools/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host "Test install"
+Write-Host "Install path: " + $installPath
+Write-Host "Tools path: " + $toolsPath
+Write-Host "Package: " + $package
+Write-Host "Project: " + $project
