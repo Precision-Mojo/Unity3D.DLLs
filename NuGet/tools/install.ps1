86154698d78cb4af26f0e700e90349c10df6a55f
Marcus R. Brown <mrbrown@precision-mojo.com>
2013-07-06 10:11:59 -0700

    Add a sentinel file and remove it during package installation.
    Use the technique described for the FluentMigrator.Tools NuGet package
    to add a sentinel file to the content/ directory, and remove it during
    installation. This forces NuGet to install the package per-project, even
    though it's technically a tools-only package.

--- NuGet/tools/install.ps1
+++ NuGet/tools/install.ps1
@@ -1,7 +1,7 @@
 param($installPath, $toolsPath, $package, $project)
 
-Write-Host "Test install"
-Write-Host "Install path: " + $installPath
-Write-Host "Tools path: " + $toolsPath
-Write-Host "Package: " + $package
-Write-Host "Project: " + $project
+# Remove the sentinel file used to ensure that the package is installed per project.
+# From http://danlimerick.wordpress.com/2011/10/01/getting-around-nugets-external-package-dependency-problem/
+$project.ProjectItems | ForEach { if ($_.Name -eq "8CFAF032-C5F2-49A3-B8F6-07EF68F4623D.txt") { $_.Remove() } }
+$projectPath = Split-Path $project.FullName -Parent
+Join-Path $projectPath "8CFAF032-C5F2-49A3-B8F6-07EF68F4623D.txt" | Remove-Item
