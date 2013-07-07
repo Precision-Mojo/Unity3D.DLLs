6467bf15d1552cd147c6a597c0180137b49be112
Marcus R. Brown <mrbrown@precision-mojo.com>
2013-07-07 10:56:32 -0700

    Remove the Unity3D.DLLs PS module if its loaded.

--- NuGet/tools/init.ps1
+++ NuGet/tools/init.ps1
@@ -1,3 +1,14 @@
+# Copyright (c) 2013 Precision Mojo, LLC.
+#
+# This file is part of the Unity3D.DLLs project (< URL >) which is distributed under the MIT License.
+# Refer to the LICENSE.MIT.md document located in the project directory for licensing terms.
+
 param($installPath, $toolsPath, $package, $project)
+
+# Remove the module if it's loaded
+if (Get-Module | ? { $_.Name -eq "Unity3D.DLLs" })
+{
+	Remove-Module Unity3D.DLLs
+}
 
 Import-Module (Join-Path $toolsPath Unity3D.DLLs.psd1)
